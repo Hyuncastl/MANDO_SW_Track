b4882129d97a5a468907220a0a12a55658167747
HYUNSUNG <118896693+Hyuncastl@users.noreply.github.com>
2023-07-19 13:47:10 +0900

    Create 1D_Array.c

---
+++ C-Programming/23_04_10/1D_Array.c
@@ -0,0 +1,17 @@
+// int*로 받고, 인덱스 접근
+
+#include<stdio.h>
+int* getNumbers() 
+{
+    static int arr[6] = {1,2,3,4,5,6};
+    return arr;
+}
+int main() 
+{
+    int* nums = getNumbers();
+    for(int i=0; i<6; i++)
+    {
+		printf("%d ", nums[i]);
+    }
+	printf("\n");
+}
