6ef17352d4035867b09a6d753de7725c86c7ce2e
HYUNSUNG <118896693+Hyuncastl@users.noreply.github.com>
2023-07-19 14:13:08 +0900

    Create EX01_2.c

---
+++ COS_Pro2/23_02_06/EX01_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+int solution(int korean, int english) {
+	int answer = 0;
+	int math = 210 - (korean + english);
+
+	if (math > 100)answer = -1;
+	else answer = math;
+	return answer;
+
+}
+
+int main() {
+	int korean = 70;
+	int english = 60;
+	int ret = solution(korean, english);
+	printf("반환값: %d\n", ret);
+}
