258a8e74d451890072d8954cdd612807aad42563
HYUNSUNG <118896693+Hyuncastl@users.noreply.github.com>
2023-07-23 15:18:51 +0900

    Create Matlab`s_ Reserved Variable.m

---
+++ MATLAB/Matlab`s_ Reserved Variable.m
@@ -0,0 +1,45 @@
+>> sqrt(-1)
+
+ans =
+
+   0.0000 + 1.0000i
+
+>> pi
+
+ans =
+
+    3.1416
+
+>> pi>inf
+
+ans =
+
+  logical
+
+   0
+
+>> pi<inf
+
+ans =
+
+  logical
+
+   1
+
+>> 0/0
+
+ans =
+
+   NaN
+
+>> inf+1
+
+ans =
+
+   Inf
+
+>> inf-1
+
+ans =
+
+   Inf
