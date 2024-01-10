bddd58c68cf0683c70e867efb102f6750af6b46c
HYUNSUNG <118896693+Hyuncastl@users.noreply.github.com>
2024-01-10 20:25:33 +0900

    Update RLC.cpp

--- C-Programming/24_01_10/RLC.cpp
+++ C-Programming/24_01_10/RLC.cpp
@@ -1,38 +1,55 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <iostream>
 #define _USE_MATH_DEFINES
 #include <cmath>
 
 
-
-int main()
+class CircuitCalculator 
 {
-	float L, Z, XC, f, Vmax, Imax, theta, VRmax, VLmax, VCmax = 0.0;
-	float R, C, XL;
+private:
+    float L, Z, XC, f, Vmax, Imax, theta, VRmax, VLmax, VCmax;
+    float R, C, XL;
 
-	L = 0.6;
-	Vmax = 150;
-	f = 60.0;
-	printf("R (Ohm): ");
-	scanf_s("%f", &R);
-	printf("C (Farad): ");
-	scanf_s("%f", &C);
+public:
+    CircuitCalculator() : L(0.6), Vmax(150), f(60.0), XC(0.0), Imax(0.0), theta(0.0), VRmax(0.0), VLmax(0.0), VCmax(0.0), R(0.0), C(0.0), XL(0.0) {}
 
-	XL = 2 * M_PI * f * L;
-	XC = 1 / (2 * M_PI * f * C);
-	Z = sqrt((R * R) + ((XL - XC) * (XL - XC)));
-	Imax = Vmax / Z;
-	theta = atan((XL - XC) / R) * (180 / M_PI);
-	VRmax = Imax * R;
-	VLmax = Imax * XL;
-	VCmax = Imax * XC;
+    void setRCValues() 
+    {
+        std::cout << "R (Ohm): ";
+        std::cin >> R;
+        std::cout << "C (Farad): ";
+        std::cin >> C;
+    }
 
-	printf("유도 리액턴스(XL): %f (Ohm) \n", XL);
-	printf("용량 리액턴스 (XC) : %f (Ohm) \n", XC);
-	printf("최대전류 (Imax) : %f(A)\n", Imax);
-	printf("임피던스 (Z) : %f (Ohm) \n", Z);
-	printf("위상각 (theta): %f (degree)\n", theta);
-	printf("VRmax : %f (Volt)\n", VRmax);
-	printf("VLmax : %f (Volt)\n", VLmax);
-	printf("VCmax : %f (Volt)\n", VCmax);
+    void calculateCircuitParameters() {
+        XL = 2 * M_PI * f * L;
+        XC = 1 / (2 * M_PI * f * C);
+        Z = sqrt((R * R) + ((XL - XC) * (XL - XC)));
+        Imax = Vmax / Z;
+        theta = atan((XL - XC) / R) * (180 / M_PI);
+        VRmax = Imax * R;
+        VLmax = Imax * XL;
+        VCmax = Imax * XC;
+    }
+
+    void displayResults() 
+    {
+        std::cout << "유도 리액턴스(XL): " << XL << " (Ohm)\n";
+        std::cout << "용량 리액턴스 (XC) : " << XC << " (Ohm)\n";
+        std::cout << "최대전류 (Imax) : " << Imax << "(A)\n";
+        std::cout << "임피던스 (Z) : " << Z << " (Ohm)\n";
+        std::cout << "위상각 (theta): " << theta << " (degree)\n";
+        std::cout << "VRmax : " << VRmax << " (Volt)\n";
+        std::cout << "VLmax : " << VLmax << " (Volt)\n";
+        std::cout << "VCmax : " << VCmax << " (Volt)\n";
+    }
+};
+
+int main() 
+{
+    CircuitCalculator circuit;
+    circuit.setRCValues();
+    circuit.calculateCircuitParameters();
+    circuit.displayResults();
+
+    return 0;
 }
